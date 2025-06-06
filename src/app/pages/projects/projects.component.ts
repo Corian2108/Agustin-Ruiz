bd3daa00e3f62ad62bf6be2d3d06695ee1e9f28c
Corian2108 <jefferson.ruiz2108@gmail.com>
2025-06-05 19:16:33 -0500

    fix/ navigation in gh pages

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 import { HeaderComponent } from "../../shared/header/header.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-projects',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
